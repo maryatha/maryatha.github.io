05325755f865c7674c00fe4bda2bcc6fa40b2b68
Mary Athappilly <maryathappilly@gmail.com>
2021-08-01 13:51:57 -0700

    m

--- js/bubble_chart.js
+++ js/bubble_chart.js
@@ -25,7 +25,7 @@ function bubbleChart() {
 
   var stateCenters = {
     Thematic: { x: width / 3, y: 2* height / 3 },
-    Strategy: { x: 3 * width / 8, y: height / 3 },
+    Strategy: { x: 3 * width / 5, y: height / 3 },
     Abstract: { x: width / 2, y: 2* height / 3 },
     Party: { x: 5 * width / 8, y: height / 3 },
     Uncategorized: { x: 2 * width / 3, y: 2* height / 3 },
